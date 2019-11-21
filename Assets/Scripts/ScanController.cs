317c3118343f62fd8ba02b80a8ea150739190a62
Andonis Gaja <andonis.gaja@tecknoworks.com>
2019-11-21 17:31:07 +0200

    add start/stop scan buttons, scripts

---
+++ Assets/Scripts/ScanController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartScan()
+    {
+        CoreServices.SpatialAwarenessSystem.ResumeObservers();
+    }
+
+    public void StopScan()
+    {
+        CoreServices.SpatialAwarenessSystem.SuspendObservers();
+    }
+}
